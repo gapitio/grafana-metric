804ecca1dcdee2de3e521aa209a88950c8c5bcbb
Ole Henrik Flesaker <ohf@gapit.io>
2020-11-18 14:34:13 +0100

    add getmetricvalue & getshowcasemetricvalue

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,30 @@ import { PanelData } from "@grafana/data";
 declare const data: PanelData;
 
 /**
+ * Generate a random number
+ *
+ * @example
+ *
+ * getShowcaseMetricValue(); // Returns a value between 0 & 1000 with 2 decimals
+ * getShowcaseMetricValue({ range: [ x, y ], decimals: z }); // Return a random value between x & y with z decimals.
+ *
+ * @param {Array<number>}range - Range of values to return
+ * @param {number}decimals - Amount of decimals returned
+ */
+function getShowcaseMetricValue({
+  range = [0, 1000],
+  decimals = 2,
+}: {
+  range?: Array<number>;
+  decimals?: number;
+} = {}): number {
+  let value = Math.random();
+  const fixedValue = (value * range[1]).toFixed(decimals);
+  value = parseFloat(fixedValue);
+  return value;
+}
+
+/**
  * Gets a metric value by name/alias
  *
  * @param {string} metricName
@@ -29,4 +53,44 @@ const getMetricValueByName = (
   return noDataValue;
 };
 
-export { getMetricValueByName };
+/**
+ * Function provides calculations for grafana queries.
+ *
+ * @example
+ *
+ * ```ts
+ * getMetricValue("queryAlias") // Returns null if query is not executed.
+ * getMetricValue("queryAlias", true); // Returns a random value between 0 and 2000000.
+ * getMetricValue("queryAlias", true, [1,10]); // Returns random value between 1-10.
+ * getMetricValue("queryAlias", true, [1,10], true); // Returns random whole value between 1-10.
+ * ```
+ *
+ * @param metric - String for alias used in grafana query.
+ * @param showcase - Decides if values are randomly generated.
+ * @param range - Range of values to return.
+ * @param decimals - Amount of decimals returned.
+ * @param noDataValue - Returns null if no data is found.
+ */
+function getMetricValue(
+  metric: string,
+  showcase = false,
+  range?: Array<number>,
+  decimals?: number,
+  noDataValue: unknown = null
+): unknown {
+  /*
+    Since the codeData is parsed as an Object in the plugin,
+    it can be used as a normal javascript dictionary.
+  */
+
+  if (showcase) {
+    return getShowcaseMetricValue({ range, decimals });
+  }
+  // else if (metric.includes('"') || metric.includes("'")) {
+  //   return getCalculationMetric(metric);
+  // }
+
+  return getMetricValueByName(metric); // , noDataValue
+}
+
+export { getMetricValueByName, getMetricValue, getShowcaseMetricValue };
